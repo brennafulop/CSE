d9692c2d864f07d2bf9d01a9fc77615911554d90
brennafulop <brennafulop@users.noreply.github.com>
2017-11-30 09:46:59 -0800

    Added Loops Random Numbers
    11.30.17

--- Notes.py
+++ Notes.py
@@ -37,56 +37,85 @@
 #functions
 
 
-def print_hw():
-    print("Hello world.")
-    print("Enjoy the day.")
+# def print_hw():
+#     print("Hello world.")
+#     print("Enjoy the day.")
+#
+#
+# print_hw()
+#
+#
+# def say_hi(name):  # Name is a "parameter"
+#     print("Hello %s" % name)
+#     print("Coding is great!")
+#
+#
+# say_hi("Brenna")  # insert any name
+#
+#
+# def print_age( name, age):
+#     print("%s is %d years old" % (name, age))  # there can only be one item after the percent. ()'s combine them
+#     age += 1 # age = age + 1   # += is the best way to add one to a digit. (+=, -=, *=, /=, %=)
+#     print("Next year, %s will be %d years old" % (name, age))
+#
+#
+# print_age("Brenna", 14)
+#
+#
+# def algebra_hw(x):
+#     return x**3 + 4*x**2 +7 * x - 4
+#
+#
+# print(algebra_hw(3))
+# print(algebra_hw(4))
+# print(algebra_hw(5))
+# print(algebra_hw(6))
+# print(algebra_hw(7))
+#
+#
+# # if statements
+#
+#
+# def grade_calc(percentage):
+#     if percentage >= 90:
+#         return "A"
+#     elif percentage >= 80: # else if
+#         return "B"
+#     elif percentage >= 70:
+#         return "C"
+#     elif percentage >= 60:
+#         return "D"
+#     else:
+#         return "F"
+#
+#
+# print(grade_calc(89))
+#
+#
+# def happy_bday(name): # def defines a function
+#     print("Happy birthday to you")
+#     print("Happy birthday to you")
+#     print("Happy birthday dear %s " %name) # also works as print("Happy birthday dear " + name)
+#     print("Happy birthday to you!")
+#
+#
+# (happy_bday("Brenna"))
 
 
-print_hw()
+# Loops
+
+# for num in range(10):
+#     print (num + 1)
+#
+# # While Loops (BEWARE)
+#
+# a = 1
+# while a < 10:
+#     print(a)
+#     a += 1 # This iterates so that we can break the loop
+
+# Random Numbers
+import random
+print(random.randint(0, 10))
 
 
-def say_hi(name):  # Name is a "parameter"
-    print("Hello %s" % name)
-    print("Coding is great!")
-
-
-say_hi("Brenna")  # insert any name
-
-
-def print_age( name, age):
-    print("%s is %d years old" % (name, age))  # there can only be one item after the percent. ()'s combine them
-    age += 1 # age = age + 1   # += is the best way to add one to a digit. (+=, -=, *=, /=, %=)
-    print("Next year, %s will be %d years old" % (name, age))
-
-
-print_age("Brenna", 14)
-
-
-def algebra_hw(x):
-    return x**3 + 4*x**2 +7 * x - 4
-
-
-print(algebra_hw(3))
-print(algebra_hw(4))
-print(algebra_hw(5))
-print(algebra_hw(6))
-print(algebra_hw(7))
-
-
-# if statements
-
-
-def grade_calc(percentage):
-    if percentage >= 90:
-        return "A"
-    elif percentage >= 80: # else if
-        return "B"
-    elif percentage >= 70:
-        return "C"
-    elif percentage >= 60:
-        return "D"
-    else:
-        return "F"
-
-
-print(grade_calc(89))
