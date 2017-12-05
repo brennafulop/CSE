3b0da29d9e1f7b6d028a361889113c6293c49b31
brennafulop <brennafulop@users.noreply.github.com>
2017-12-05 09:47:19 -0800

    Warmups + Working Guessgame
    12.5.17

--- Brenna - Guessgame.py
+++ Brenna - Guessgame.py
@@ -2,21 +2,29 @@
 # OF TYPE STRING!!!!
 
 import random
-
+correct_guess = False
 
 winning_number = random.randint(1,50)
 guesses = 5
-
+print(winning_number)
 
 
 number = int(input("Choose a number between one and fifty."))
-if number == winning_number:
-    print("Congrats! You win! The winning number was %d. You had %d guesses left." % (winning_number, guesses))
-elif number > winning_number:
-    guesses -= 1
-    print("Your guess is too high! You only have %d guesses left!" % guesses)
-elif number < winning_number:
-    guesses -= 1
-    print("Your guess is too low! You only have %d guesses left!" % guesses)
 
+
+while guesses != 0 and correct_guess == False:
+    if number == winning_number:
+        guesses -= 1
+        print("Congrats! You win! The winning number was %d. You had %d guesses left." % (winning_number, guesses))
+        correct_guess = True
+    elif number > winning_number:
+        guesses -= 1
+        print("Your guess is too high! You only have %d guesses left!" % guesses)
+        number = int(input("Guess again!"))
+    elif number < winning_number:
+        guesses -= 1
+        print("Your guess is too low! You only have %d guesses left!" % guesses)
+        number = int(input("Guess again!"))
+if guesses == 0:
+    print("You lose! Better luck next time!")
 
--- Warmups.py
+++ Warmups.py
@@ -6,11 +6,23 @@
 # and print them in reverse
 
 
-def reverse_order(first_name, last_name):
-    print("%s, %s" % (last_name, first_name))
-    # or print(last_name + " " + first_name) #concatenation
+# def reverse_order(first_name, last_name):
+#     print("%s, %s" % (last_name, first_name))
+#     # or print(last_name + " " + first_name) #concatenation
+#
+# first = input('what is your first name?')
+# last = input('what is your last name?')
+#
+# reverse_order(first, last)
 
-first = input('what is your first name?')
-last = input('what is your last name?')
 
-reverse_order(first, last)
+# 12.5.17
+'''Write a function called add_py
+that takes one parameter called "name"
+and prints out name.py'''
+
+
+def add_py (name):
+    print(name + ".py")
+
+add_py("brenna")
