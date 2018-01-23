50d0fe419e903f0ddaffb8d35a2910b819450b4f
brennafulop <31043482+brennafulop@users.noreply.github.com>
2018-01-23 09:47:52 -0800

    1.23.18
    worked on hangman

--- Brenna Fulop - Hangman.py
+++ Brenna Fulop - Hangman.py
@@ -1,24 +1,44 @@
 import random
 import string
+
 """
+
 This is a guide of how to make hangman
 
 1. Make a word bank - 10 items
 2. Select a random item to guess
-3. Hide the word (use *)
-4. Reveal letters based on input
+3. Take in a letter and add it to a list of letters_guessed
+4. Hide and reveal letters
 5. Create win and lose condition
+
 """
 
 word_bank = ['forest', 'melancholy', 'pluto', 'strange', 'infinity', 'goodbye', 'river', 'interestingly', 'canada',
              'fascinating']
+guesses_left = 10
 
 random_word = random.choice(word_bank)
 list_word = list(random_word)
 print(list_word)
+
 hidden_word = list("*" * len(list_word))
 print(hidden_word)
 print("The word is " + str(len(list_word)), "letters long.")
 print(string.ascii_lowercase)
-user_input = input("Guess a letter >_")
-list_word.index(user_input)
+
+guesses = []
+
+while guesses_left > 0:
+    current_guess = input("Guess a letter >_")
+    guesses.append(current_guess)
+    print(guesses)
+    output = []
+    if current_guess == 'quit':
+        exit(0)
+    for letter in random_word:
+        if letter in guesses:
+            output.append(letter)
+        else:
+            output.append('*')
+    print(output)
+
