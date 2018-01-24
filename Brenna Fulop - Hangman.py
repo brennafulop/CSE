3aca91840f2fb03ba529863ddb26a9add2a4c4f2
brennafulop <31043482+brennafulop@users.noreply.github.com>
2018-01-24 09:47:55 -0800

    1.24.17
    Almost finished hangman

--- Brenna Fulop - Hangman.py
+++ Brenna Fulop - Hangman.py
@@ -1,5 +1,6 @@
 import random
 import string
+import sys
 
 """
 
@@ -13,32 +14,36 @@ This is a guide of how to make hangman
 
 """
 
-word_bank = ['forest', 'melancholy', 'pluto', 'strange', 'infinity', 'goodbye', 'river', 'interestingly', 'canada',
-             'fascinating']
+word_bank = ['Forest', 'Melancholy', 'Pluto', 'Strange', 'Infinity', 'Goodbye', 'River', 'Interestingly', 'Canada',
+             'Fascinating']
 guesses_left = 10
-
 random_word = random.choice(word_bank)
 list_word = list(random_word)
-print(list_word)
 
-hidden_word = list("*" * len(list_word))
-print(hidden_word)
+print('*' * len(random_word))
 print("The word is " + str(len(list_word)), "letters long.")
-print(string.ascii_lowercase)
+print("You have 10 guesses.")
 
 guesses = []
 
 while guesses_left > 0:
     current_guess = input("Guess a letter >_")
     guesses.append(current_guess)
-    print(guesses)
-    output = []
     if current_guess == 'quit':
         exit(0)
+    if current_guess not in random_word:
+        guesses_left -= 1
+    print("You have %s guesses left" % guesses_left)
+    output = []
     for letter in random_word:
         if letter in guesses:
             output.append(letter)
         else:
             output.append('*')
+    output = ''.join(output)
+    if '*' not in output:
+        print('You win! The word was %s' % random_word)
+        sys.exit()
     print(output)
 
+print('You lose! The word was %s' % random_word)
