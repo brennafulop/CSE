50f02e4a5dfc9a6b501e4b4d4b8caee77150a09f
brennafulop <31043482+brennafulop@users.noreply.github.com>
2018-02-06 09:46:26 -0800

    2.6.18
    worked on notes

--- Notes.py
+++ Notes.py
@@ -200,12 +200,14 @@ print(dictionary['name'])
 print(dictionary['age'])
 print(dictionary['height'])
 
+# Add a pair to a dictionary
+dictionary['profession'] = 'telemarketer'
+
 large_dictionary = {
     'CA': 'California',
     'AZ': 'Arizona',
     'NY': 'New York'
 }
-
 print(large_dictionary['NY'])
 
 larger_dictionary = {
@@ -223,4 +225,42 @@ larger_dictionary = {
         'Brooklyn'
     ]
 }
+print(larger_dictionary['NY'])
 print(larger_dictionary['NY'][1])
+print(larger_dictionary['AZ'][0])
+
+largest_dictionary = {
+    'CA': {
+        "NAME": "California",
+        'POPULATION': 39250000,
+        'BORDER ST': [
+            'Oregon',
+            'Nevada',
+            'Arizona'
+        ]
+    },
+    "AZ": {
+        'NAME': "Arizona",
+        'POPULATION': '6931000',
+        "BORDER ST": [
+            'California',
+            'Utah',
+            'Nevada',
+            'New Mexico'
+        ]
+    },
+    "NY": {
+        'NAME': 'New York',
+        'POPULATION': 19750000,
+        'BORDER ST': [
+            'Vermont',
+            'Massachusetts',
+            'Connecticut',
+            "Pennsylvania",
+            'New Jersey'
+        ]
+    }
+}
+current_node = largest_dictionary['NY']
+print(current_node['NAME'])
+print(current_node["POPULATION"])
