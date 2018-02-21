cdfbc0a8b17f783b2c99f19c680f100fcb48aee7
brennafulop <31043482+brennafulop@users.noreply.github.com>
2018-02-21 09:46:41 -0800

    2.21.18
    added new files and added notes

---
+++ Brenna Fulop - Pythagorean.py
@@ -0,0 +1,6 @@
+def pythagorean_calc(a, b):
+    inside = (a ** 2 + b ** 2)
+    c = inside ** 0.5
+    return c
+
+print(pythagorean_calc(5, 12))
---
+++ Class Notes.py
@@ -0,0 +1,39 @@
+# Defining a class
+class Cheeseburger(object):
+    def __init__(self, patty, cheese):
+        self.patty = patty
+        self.cheese = cheese
+        self.eaten = False
+
+    def give(self, name):
+        print(name + "is thankful")
+
+    def cut(self):
+        print('You cut the cheeseburger')
+
+    def eat(self):
+        print('You eat the cheeseburger')
+        self.eaten = True
+
+
+# Instantiating two cheeseburgers
+burger1 = Cheeseburger("Beef", "Havarti")
+burger2 = Cheeseburger("Bacon", 'Swiss')
+
+print(burger1.eaten)
+print(burger2.cheese)
+
+burger1.eat()
+print(burger1.eaten)
+print(burger2.eaten)
+
+
+class car(object):
+    def __init__(self, name, color, num_of_doors, hp):
+        self.color = color
+        self.num_of_doors = num_of_doors
+        self.running = False
+        self.HP = hp
+        self.passengers = 0
+        self.name = name
+        
