3e70cf4fae9170fa377d713aa5735253e983127d
brennafulop <31043482+brennafulop@users.noreply.github.com>
2018-03-16 10:39:13 -0700

    3.16.18
    worked on items, finished superclasses

--- Brenna Fulop - Items.py
+++ Brenna Fulop - Items.py
@@ -19,10 +19,41 @@ class Item(object):
         room.inventory.remove(self)
         person.inventory.append(self)
 
+
 class Weapon(Item):
     def __init__(self, name, description, damage):
         super(Weapon, self).__init__(name, description)
         self.damage = damage
+
+class Melee(Weapon):
+    def __init__(self, name, description, damage):
+        super(Melee, self).__init__(name, description, damage)
+        self.damage = damage
+
+
+class Sword(Melee):
+    def __init__(self, name, description, damage):
+        super(Sword, self).__init__(name, description, 10)
+
+
+class Knife(Melee):
+    def __init__(self, name, description, damage):
+        super(Knife, self).__init__(name, description, 10)
+
+
+class ElvenSword(Sword):
+    def __init__(self, name, description, damage):
+        super(ElvenSword, self).__init__(name, description, 15)
+
+
+class Ranged(Weapon):
+    def __init__(self, name, description, damage, distance):
+        super(Ranged, self).__init__(name, description, damage)
+
+
+class Bow(Ranged):
+    def __init__(self, name, description, damage, distance):
+        super(Bow, self).__init__('Bow', description, 15, 30)
 
 
 class Wearable(Item):
@@ -44,6 +75,27 @@ class Consumable(Item):
     def __init__(self, name, description):
         super(Consumable, self).__init__(name, description)
 
-hm = Item("HH", "hh")
-HHHHH = Room([hm])
-print(HHHHH.items[0].name)
+
+class Food(Consumable):
+    def __init__(self, name, description, nutrients):
+        super(Food, self).__init__(name, description)
+
+
+class Dried_Meat(Food):
+    def __init__(self, name, description, nutrients):
+        super(Dried_Meat, self).__init__(name, description, 10)
+
+
+class Lembas(Food):
+    def __init__(self, name, description, nutrients):
+        super(Lembas, self).__init__(name, description, 5)
+
+
+class Liquid(Consumable):
+    def __init__(self, name, description):
+        super(Liquid, self).__init__(name, description)
+
+
+class Water(Liquid):
+    def __init__(self, name, description):
+        super(Water, self).__init__(name, description)
