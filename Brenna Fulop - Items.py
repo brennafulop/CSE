4b62357453707f1f7e07d62cb679e7466eec3e25
brennafulop <31043482+brennafulop@users.noreply.github.com>
2018-03-20 09:45:20 -0700

    3.20.18
    worked on items, tested inventories and equip

--- Brenna Fulop - Items.py
+++ Brenna Fulop - Items.py
@@ -1,15 +1,17 @@
 class Room(object):
-    def __init__(self, items = None):
+    def __init__(self, items=None):
         self.items = items
 
 
 class Person(object):
-    def __init__(self, items = None):
-        if items is None:
-            items = []
+    def __init__(self, items=None):
         self.inventory = items
         self.cloak_slot = None
         self.helm = None
+
+    def pick_up(self, item, room):
+        room.items.remove(item)
+        self.inventory.append(item)
 
     def equip(self, item):
         if isinstance(item, Helmet):
@@ -17,7 +19,6 @@ class Person(object):
             item.put_on()
         elif isinstance(item, Cloak):
             self.cloak_slot = item
-            item.put_on()
 
 
 class Item(object):
@@ -26,13 +27,8 @@ class Item(object):
         self.description = description
 
     def picked_up(self, person, room):
-        room.inventory.remove(self)
+        room.items.remove(self)
         person.inventory.append(self)
-
-
-class Pebble(Item):
-    def __init__(self, name, description):
-        super(Pebble, self).__init__("Pebble", "A small blue pebble. It has a strange gleam to it.")
 
 
 class Weapon(Item):
@@ -49,27 +45,23 @@ class Melee(Weapon):
 
 class Sword(Melee):
     def __init__(self, name, description, damage):
-        super(Sword, self).__init__('Sword', 'An old rusted sword, it appears fairly dull.', 15)
+        super(Sword, self).__init__(name, description, damage)
 
 
 class Knife(Melee):
     def __init__(self, name, description, damage):
-        super(Knife, self).__init__('Knife', 'A small sharp knife.', 10)
-
-
-class ElvenSword(Sword):
-    def __init__(self, name, description, damage):
-        super(ElvenSword, self).__init__('Elven sword', 'A very sharp Elven sword. It has jewels in the handle.', 20)
+        super(Knife, self).__init__(name, description, damage)
 
 
 class Ranged(Weapon):
     def __init__(self, name, description, damage, distance):
         super(Ranged, self).__init__(name, description, damage)
+        self.distance = distance
 
 
 class Bow(Ranged):
     def __init__(self, name, description, damage, distance):
-        super(Bow, self).__init__('Bow', 'A wooden bow. It appears to be in pretty good shape.', 15, 30)
+        super(Bow, self).__init__(name, description, damage, distance)
 
 
 class Wearable(Item):
@@ -85,14 +77,14 @@ class Wearable(Item):
             self.wearing = False
 
 
-class Cloak(Wearable):
-    def __init__(self, name, description):
-        super(Cloak, self).__init__('Desert Cloak', 'A cloak that would protect you from the forces of the desert')
-
-
 class Helmet(Wearable):
     def __init__(self, name, description):
-        super(Helmet, self).__init__('Helmet', 'A slightly rusty helmet, it appears strong despite its wear.')
+        super(Helmet, self).__init__(name, description)
+        
+        
+class Cloak(Wearable):
+    def __init__(self, name, description):
+        super(Cloak, self).__init__(name, description)
 
 
 class Container(Item):
@@ -100,16 +92,7 @@ class Container(Item):
         super(Container, self).__init__(name, description)
         if items is None:
             items = []
-
-
-class Bottle(Container):
-    def __init__(self, name, description):
-        super(Bottle, self).__init__('Bottle', 'A clear glass bottle')
-
-
-class Chest(Container):
-    def __init__(self, name, description):
-        super(Chest, self).__init__('Chest', 'A large wooden chest.')
+        self.inventory = items
 
 
 class Consumable(Item):
@@ -120,16 +103,7 @@ class Consumable(Item):
 class Food(Consumable):
     def __init__(self, name, description, nutrients):
         super(Food, self).__init__(name, description)
-
-
-class Dried_Meat(Food):
-    def __init__(self, name, description, nutrients):
-        super(Dried_Meat, self).__init__(name, description, 10)
-
-
-class Lembas(Food):
-    def __init__(self, name, description, nutrients):
-        super(Lembas, self).__init__(name, description, 5)
+        self.nutrients = nutrients
 
 
 class Liquid(Consumable):
@@ -137,6 +111,11 @@ class Liquid(Consumable):
         super(Liquid, self).__init__(name, description)
 
 
-class Water(Liquid):
-    def __init__(self, name, description):
-        super(Water, self).__init__(name, description)
+tester = Person([])
+desert_cloak = Cloak('Desert Cloak', '8')
+tester.cloak_slot = desert_cloak
+tester.equip(desert_cloak)
+
+elven_sword = Sword('Elven Sword', 'An expensive looking sword. It is sharp, with jewels in the handle', 20)
+tester_room = Room([elven_sword])
+print(tester_room.items[0].name)
